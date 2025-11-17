bbb564d936516e96829e6c19b0cfe9964d190807
Dhairya05 <dhairyathaker61@gmail.com>
2025-11-17 18:32:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_23012021007_practical_6/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mad_23012021007_practical_6
+
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var alarmAnimation: AnimationDrawable
+    private lateinit var heartAnimation: AnimationDrawable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val alarmImageView = findViewById<ImageView>(R.id.imageView)
+        alarmAnimation = alarmImageView.drawable as AnimationDrawable
+
+        val heartImageView = findViewById<ImageView>(R.id.imageView3)
+        heartAnimation = heartImageView.drawable as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            alarmAnimation.start()
+            heartAnimation.start()
+        } else {
+            alarmAnimation.stop()
+            heartAnimation.stop()
+        }
+    }
+}
